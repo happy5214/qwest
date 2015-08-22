0d96b5f9746d7dbdd5cc7a373293a967b32059f0
Thomas Ritschel <thomas.ritschel@googlemail.com>
2015-08-22 06:02:32 +0200

    Delete hardcoded primes list and use nmap instead of rpntab

--- qwest.c
+++ qwest.c
@@ -8,27 +8,11 @@
 #include <unistd.h>
 #include <string.h>
 
-#define maxnprimes 1000
-#define maxn  1000
-
-/*
-int primes[] = {  2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
-                 31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
-                 73,  79,  83,  89,  97, 101, 103, 107, 109, 113,
-                127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
-                179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
-                233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-                283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
-                353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
-                419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
-                467, 479, 487, 491, 499, 503, 509 };
-*/
-int primes[maxnprimes];
-
-int plist[maxnprimes];
-int otable[maxnprimes];
-int o1list[maxnprimes];    // list of primes with ord(p,b) = 1
-int nmodp[maxn][maxnprimes];    // precomputed values of n%p
+int *primes;
+int *plist;
+int *otable;
+int *o1list;    // list of primes with ord(p,b) = 1
+int *nmap;      // array of n for which p | k*b^n-1
 int b = 2;
 int nplist = 0;
 int opmax = 0;
@@ -36,7 +20,7 @@ int o1max = 0;
 int nprimes;
 int maxp = 512;
 int maxord = 512;
-unsigned int *rpntab;
+int maxn = 1000;
 uint64_t kmin, kmax, kstep;
 int low, high;
 FILE *zerofile;
@@ -46,7 +30,7 @@ FILE *highfile;
 int Erathosthenes(int pmax)
 {
   int numbers[pmax];
-  int i, j;
+  int i, j, count;
   for (i=0; i<pmax-1; i++)
     numbers[i] = i+2;
 // sieve
@@ -54,6 +38,12 @@ int Erathosthenes(int pmax)
     if (numbers[i] > 0)
       for (j=2*numbers[i]-2; j<pmax; j+=numbers[i])
         numbers[j] = 0;
+// count the primes
+  count = 0;
+  for (i=0; i<pmax-1; i++)
+    if (numbers[i] > 0)
+      count++;
+  primes = (int *) calloc(count, sizeof(int));
 // transfer the primes to their own array
   j = 0;
   for (i=0; i<pmax-1; i++)
@@ -98,6 +88,9 @@ void init_plist(void)
   int count = 0;
   int ocount = 0;
   int o1count = 0;
+  plist = (int *) calloc(nprimes, sizeof(int));
+  otable = (int *) calloc(nprimes, sizeof(int));
+  o1list = (int *) calloc(nprimes, sizeof(int));
   for (i=0; i<nprimes; i++)
   {
     p = primes[i];
@@ -123,13 +116,11 @@ void init_plist(void)
   printf("o1max = %d\n", o1max);
 }
 
-void init_rpntab(void)
+void init_nmap(void)
 {
-  int i, j, k, n, p;
-  unsigned int *pntab;
-  pntab = (unsigned int *) calloc(opmax+1, sizeof(unsigned int));
-  rpntab = (unsigned int *) calloc(opmax+1, sizeof(unsigned int));
-  if ((pntab == NULL) || (rpntab == NULL))
+  int i, j, k, n, p, m;
+  nmap = (int *) calloc(nprimes*maxp, sizeof(int));
+  if (nmap == NULL)
   {
     printf("memory allocation error!\n");
     exit(1);
@@ -144,27 +135,12 @@ void init_rpntab(void)
     {
 //      printf("k = %d p = %d otable(%d) = %d\n", k, p, i, otable[i]);
       k = (b*k)%p;
+      m = otable[i]-n-1;
+      if (m == 0)
+        m+=otable[i];
+      nmap[i*maxp+k] = m;
       j++;
-      pntab[j] = k;
-//      printf("pntab(%d) = %d\n", j, k);
     }
-    rpntab[j] = pntab[j];
-    for (n=1; n<otable[i]; n++)
-      rpntab[j-n] = pntab[j-otable[i]+n];
-  }
-  rpntab[0] = 1;
-// rpntab[opmax] = 1;
-  free(pntab);
-}
-
-void init_nmodp(void)
-{
-  int i, n, p;
-  for (i=0; i<nplist; i++)
-  {
-    p = plist[i];
-    for (n=0; n<maxn; n++)
-      nmodp[n][i] = n%p;
   }
 }
 
@@ -190,7 +166,7 @@ void close_files(void)
 void sieve(void)
 {
   uint64_t k;
-  int i, j, l, m, n, o, p, om;
+  int i, j, l, m, n, o, p, om, nm;
   int count;
   int kmodp[nplist];    // precomputed k%p for all p in plist
   int kbmodp[o1max];    // precomputed (k*b^1)%p for all p in o1list
@@ -263,7 +239,6 @@ void sieve(void)
 //      for (n=0; n<maxn; n++)
 //        remain[n] = true;
       memcpy(remain, full_remain, maxn*sizeof(bool));
-      j = 0;
       for (i=0; i<nplist; i++)
       {
         p = plist[i];
@@ -271,21 +246,11 @@ void sieve(void)
         ks = kmodp[i];
         if (ks > 0)
         {
-          om = maxn;
-          if (o < maxn)
-            om = o+1;
-          for (n=1; n<om; n++)
-          {
-            m = nmodp[n][i];
-            if (ks == rpntab[j+m])
-            {
-              for (l=n; l<=maxn; l+=o)
-                remain[l-1] = false;
-              break;
-            }
-          }
+          n = nmap[i*maxp+ks];
+          if (n > 0)
+            for (l=n; l<=maxn; l+=o)
+              remain[l-1] = false;
         }
-        j += o;
       }
       count = 0;
       for (n=0; n<maxn; n++)
@@ -325,8 +290,9 @@ int main(int argc, char *argv[])
   high   =     334;
   maxp   =     512;
   maxord =     512;
+  maxn   =    1000;
 
-  while ((option = getopt(argc, argv, "b:k:K:s:l:h:p:o:")) >= 0)
+  while ((option = getopt(argc, argv, "b:k:K:s:l:h:p:o:n:")) >= 0)
     switch (option)
     {
       case 'b' : b = atoi(optarg);
@@ -345,6 +311,8 @@ int main(int argc, char *argv[])
                  break;
       case 'o' : maxord = atoi(optarg);
                  break;
+      case 'n' : maxn = atoi(optarg);
+                 break;
       case '?' : return 1;
     }
 
@@ -353,6 +321,7 @@ int main(int argc, char *argv[])
   nprimes = Erathosthenes(maxp);
   printf("no. of primes = %d\n", nprimes);
   printf("largest prime = %d\n", primes[nprimes-1]);
+  maxp = primes[nprimes-1];
 
 /*
   int i;
@@ -362,8 +331,18 @@ int main(int argc, char *argv[])
 */
    
   init_plist();
-  init_rpntab();
-  init_nmodp();
+  init_nmap();
+
+/*
+  int i, j;
+  for (i=0; i<nprimes; i++)
+  {
+    for (j=0; j<maxp; j++)
+      printf("%3d", nmap[i*maxp+j]);
+    printf("\n");
+  }
+*/
+
   open_files();
   sieve();
   close_files();
