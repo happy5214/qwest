cbfd82d159d17523ade5fbe98c9c52ec08448170
Alexander Jones <happy5214@gmail.com>
2024-04-01 07:39:15 -0500

    Make formatting consistent

--- int128.c
+++ int128.c
@@ -6,11 +6,13 @@
 
 #define P10_UINT64 10000000000000000000ULL
 
-uint128_t strtou128(const char *string) {
+uint128_t strtou128(const char *string)
+{
   const uint128_t base = 10;
   uint128_t number = 0;
 
-  for(; *string; ++string) {
+  for(; *string; ++string)
+  {
     unsigned char digit = *string;
     digit -= '0';
     number *= base;
@@ -22,22 +24,29 @@ uint128_t strtou128(const char *string) {
   return number;
 }
 
-int snprint_u128(char *buffer, size_t size, const uint128_t u128) {
+int snprint_u128(char *buffer, size_t size, const uint128_t u128)
+{
   int charactersWritten = 0;
 
-  if (u128 > UINT64_MAX) {
+  if (u128 > UINT64_MAX)
+  {
     const uint128_t leading = u128 / P10_UINT64;
     const uint64_t trailing = u128 % P10_UINT64;
 
-    if (leading > UINT64_MAX) {
+    if (leading > UINT64_MAX)
+    {
       const uint64_t u64Leading = leading / P10_UINT64;
       const uint64_t u64Trailing = leading % P10_UINT64;
       charactersWritten = snprintf(buffer, size, "%" PRIu64 "%.19" PRIu64 "%.19" PRIu64, u64Leading, u64Trailing, trailing);
-    } else {
+    }
+    else
+    {
       const uint64_t u64 = leading;
       charactersWritten = snprintf(buffer, size, "%" PRIu64 "%.19" PRIu64, u64, trailing);
     }
-  } else {
+  }
+  else
+  {
     const uint64_t u64 = u128;
     charactersWritten = snprintf(buffer, size, "%" PRIu64, u64);
   }
